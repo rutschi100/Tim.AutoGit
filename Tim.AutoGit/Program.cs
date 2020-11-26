5b362fb64bbb83827866054e46964dd7e4613e1a
Tim Rutschi <timrutschi@gmx.ch>
2020-11-26 18:10:00 +0100

    Create Project

---
+++ Tim.AutoGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tim.AutoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
